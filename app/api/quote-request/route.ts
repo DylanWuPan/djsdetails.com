a63dd99db67bacd781f6c55c9e72abe6511d5f7b
Dylan Pan <dylanpan56@gmail.com>
2026-05-06 19:46:48 -0400

    fix api key

--- app/api/quote-request/route.ts
+++ app/api/quote-request/route.ts
@@ -1,7 +1,13 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const API_KEY = process.env.RESEND_API_KEY;
+
+if (!API_KEY) {
+  throw new Error("RESEND_API_KEY is not defined in environment variables.");
+}
+
+const resend = new Resend(API_KEY);
 
 export async function POST(req: Request) {
   try {
